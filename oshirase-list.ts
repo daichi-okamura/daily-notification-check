bee444f4e4519050060a33e768bb980017803555
daichi-okamura <daichi-okamura@bm-sms.co.jp>
2022-02-26 02:35:38 +0900

    前回の結果を保存して今回の結果と比較し差分を表示するように仕様を変更する

--- oshirase-list.ts
+++ oshirase-list.ts
@@ -1,20 +1,26 @@
-/*
- * 電子請求受付システムにログインしなくても閲覧可能なお知らせのタイトル一覧を取得する
- */
-import puppeteer from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
+import puppeteer, {Browser, Page} from "https://deno.land/x/puppeteer@9.0.2/mod.ts";
+import {exists} from "https://deno.land/std/fs/mod.ts";
 
-const browser = await puppeteer.launch();
-const page = await browser.newPage();
+const OLD_JSON = "./old.json";
+const fetchOld = async () => await exists(OLD_JSON) ? JSON.parse(Deno.readTextFileSync(OLD_JSON)) : []
+const fetchNow = async () => {
+    const browser: Browser = await puppeteer.launch();
+    const page: Page = await browser.newPage();
+    await page.goto('http://www.e-seikyuu.jp/');
+    await page.goto('https://www.kaigo.e-seikyuu.jp/KShinsei/main');
+    const selector: string = 'table.bg-blue01 .bc-blue01 > tbody > tr > td:nth-child(2) > span > a';
+    const notifications = await page.$$eval(selector, a => a.map(b => b.text));
+    await browser.close();
+    return JSON.parse(JSON.stringify(notifications));
+}
 
-await page.goto("http://www.e-seikyuu.jp/");
-await page.goto("https://www.kaigo.e-seikyuu.jp/KShinsei/main");
+const diff = async () => {
+    const now = await fetchNow();
+    const old = await fetchOld();
+    const result: string[] = [];
+    for (let n of now) if (old.indexOf(n) < 0) result.push(n);
+    Deno.writeTextFileSync(OLD_JSON, JSON.stringify(now));
+    return result;
+}
 
-const selector = 'table.bg-blue01 .bc-blue01 > tbody > tr > td:nth-child(2) > span > a';
-
-const notifications = await page.$$eval(selector, items => {
-    return items.map(item => item.textContent);
-});
-
-console.log(notifications);
-
-await browser.close();
+console.log(JSON.stringify(await diff()))
